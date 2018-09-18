084c6ced329b94d3416460f0cfcd820c5603b637
Peter Pohlmann <ppoh71@gmail.com>
2018-09-19 00:14:01 +0200

    print statements deleted

--- mememe/TextInputDelegate.swift
+++ mememe/TextInputDelegate.swift
@@ -10,11 +10,10 @@ import Foundation
 import UIKit
 
 class TextInputDelegate: NSObject, UITextFieldDelegate{
-    //we want to keep the old text when the user deletes all text and pressed return
+    //keep the old text when user deleted all text and pressed return
     var oldText = ""
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        print("text delegate")
         oldText = textField.text!
         
         if (textField.text! == "TOP" || textField.text! == "BOTTOM") {
@@ -23,21 +22,18 @@ class TextInputDelegate: NSObject, UITextFieldDelegate{
     }
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        //make the text uppercase not matter what, had some problems with the shift-key on the keyboard
+        //make the text uppercase not matter what
         textField.text = (textField.text! as NSString).replacingCharacters(in: range, with: string.uppercased())
         return false
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        print("end editing")
         if textField.text?.count == 0{
             textField.text = oldText
         }
-        
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        print("should retrun")
         textField.endEditing(true)
         return false
     }
